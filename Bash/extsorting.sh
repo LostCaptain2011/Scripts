aa019e276617a9204cb614a4018a6a940803bbcf
Nicholas Perry <nuperry2011@gmail.com>
2023-05-29 15:41:41 -0700

    First script

---
+++ Bash/extsorting.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+linuxDir=~/winDesktop/Desktop/LinuxFolder/
+
+for filename in "$linuxDir"*; do
+     base=${filename%.*}
+     ext=${filename#"$base".}
+     extDir=${linuxDir}${ext}
+     echo "$base"
+     # echo "$ext"
+     # echo "$filename"
+     if [ -d "$extDir" ]; then
+          echo "moving $filename to $extDir"
+          mv "${filename}" "${extDir}"
+     else
+          echo "$extDir doesn't exist, creating now"
+          mkdir "$extDir"
+          echo "moving $filname to $extDir"
+          mv "${filename}" "${extDir}"
+     fi
+done
+
+# linuxDir
